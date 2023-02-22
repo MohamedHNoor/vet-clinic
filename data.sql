32896e8680c5d03f3c3afef96baa9bd98c23d089
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-02-22 10:23:10 +0200

    Inser data into owners table, species table, modified animals with species_id and ower_id

--- data.sql
+++ data.sql
@@ -160,3 +160,95 @@ INSERT INTO animals (
   'true',
   22
 );
+
+-- owners table
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Sam Smith',
+  34
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Jennifer Orwell',
+  19
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Bob',
+  45
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Melody Pond',
+  77
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Dean Winchester',
+  14
+);
+
+INSERT INTO owners (
+  full_name,
+  age
+) VALUES (
+  'Jodie Whittaker',
+  38
+);
+
+-- species table
+INSERT INTO species (
+  name
+) VALUES (
+  'Digimon'
+);
+
+INSERT INTO species (
+  name
+) VALUES (
+  'Pokemon'
+);
+
+-- Modified inserted animsls table with species_id
+UPDATE animals
+SET species_id = 1
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 2
+WHERE species_id IS NULL;
+
+-- Modified inserted animsls table with owner_id
+UPDATE animals
+SET owner_id = 1
+WHERE name IN ('Agumon');
+
+UPDATE animals
+SET owner_id = 2
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = 3
+WHERE name IN ('Plantmon');
+
+UPDATE animals
+SET owner_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = 5
+WHERE name IN ('Boarmon', 'Angemon');
