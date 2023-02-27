fd3112089a7b341c7ab5f761b49eaa4bf448367a
admirerbrown <admirerbrown@gmail.com>
2023-02-27 13:15:20 +0000

    insert queries into queries.sql file

--- queries.sql
+++ queries.sql
@@ -211,3 +211,7 @@ WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name
 ORDER BY COUNT(*) DESC
 LIMIT 1;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
